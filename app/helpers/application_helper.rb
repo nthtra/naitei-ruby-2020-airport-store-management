be35a0eb0bbc45d5fcdaddc3faf1c7a6cc0b5c53
kguen-sun <nguyen.anh.khoa@sun-asterisk.com>
2020-08-19 13:28:12 +0700

    [TASK][27013] User signup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def disable_layout?
+    current_page?(signup_path) || current_page?(login_path) ||
+      (controller.controller_name == "users" &&
+        controller.action_name == "create") ||
+      (controller.controller_name == "sessions" &&
+        controller.action_name == "create")
+  end
 end
