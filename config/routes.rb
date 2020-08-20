6a9d9e6de8cdaf498b29d9aab84e456d574aca46
kguen-sun <nguyen.anh.khoa@sun-asterisk.com>
2020-08-20 07:49:23 +0700

    [TASK][27019] User logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
-
     get "/signup", to: "users#new"
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
