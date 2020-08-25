d32f98754e98b9bc5aa66a177108d28791ef29e1
huongtrauet <huongtrauet@gmail.com>
2020-08-25 14:37:46 +0700

    task_27808

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -1,18 +1,12 @@
 class SlotsController < ApplicationController
-  before_action :find_employee
+  # before_action :find_employee
+
+  def show
+    @slot = Slot.find_by(id: params[:id])
+  end
 
   def index
     @slots = Slot.by_status_and_datetime.page(params[:page])
                  .per Settings.paginate.slots_per_page
-  end
-
-  private
-
-  def find_employee
-    @employee = Employee.find_by id: session[:employee_id]
-    return if @employee
-
-    flash[:error] = t ".employee_not_found"
-    redirect_to employees_login_path
   end
 end
