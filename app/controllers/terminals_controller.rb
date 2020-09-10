e0d4a852c13aa00f9b8afa75ea1984030ad81ed6
nnhai-naitei-intern <nguyen.nhat.hai@sun-asterisk.com>
2020-09-10 10:50:02 +0700

    [Task][27050][User] Search slot

--- app/controllers/terminals_controller.rb
+++ app/controllers/terminals_controller.rb
@@ -1,7 +1,8 @@
 class TerminalsController < ApplicationController
   def show
     @terminal = Terminal.find_by id: params[:id]
-    @slots = @terminal.slots.by_status_and_datetime_for_user
-                      .page(params[:page]).per Settings.pagination.per_page
+    @qt = @terminal.slots.ransack params[:q]
+    @slots = @qt.result.by_status_and_datetime_for_user
+                .page(params[:page]).per Settings.pagination.per_page
   end
 end
---
+++ config/initializers/ransack.rb
@@ -0,0 +1,14 @@
+Ransack.configure do |config|
+  config.add_predicate "between",
+                       arel_predicate: "between",
+                       formatter: proc { |v| v.split(" to ") },
+                       type: :string
+end
+
+module Arel
+  module Predications
+    def between other
+      gteq(other[0]).and(lteq(other[1]))
+    end
+  end
+end
